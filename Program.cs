ffb52c61d1c0e716b3ef7d30f89643331127cd87
aousjosef <aoussabah@gmail.com>
2023-12-28 20:26:32 +0100

    moment 3 C#

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿namespace m3;
+using Dagbok;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        DagBokClass dagbok = new DagBokClass();
+
+        //   Console.WriteLine(dagbok.aj);
+
+        void showOptions()
+        {
+            Console.WriteLine("welcome to dagbok, please choose an option: ");
+            Console.WriteLine("1. Show all notes. ");
+            Console.WriteLine("2. Add a new note. ");
+            Console.WriteLine("3. Delete a note. ");
+            Console.WriteLine("4. Exit program");
+        }
+
+
+        showOptions();
+
+        while (true)
+        {
+            var option = Console.ReadLine();
+
+            if (int.TryParse(option, out int num))
+            {
+
+
+                switch (num)
+                {
+
+                    //Case 1 visa all  notes
+                    case 1:
+                        Console.WriteLine("You entered 1. Here are all the notes");
+                        dagbok.showAllNotes();
+
+                        break;
+
+
+                    //Skriv en ny note
+                    case 2:
+                        Console.WriteLine("You entered 2. please enter your name: ");
+
+                        string author = Console.ReadLine();
+
+                        Console.WriteLine("You entered 2. please enter your note: ");
+
+                        string content = Console.ReadLine();
+
+
+                        if (!string.IsNullOrWhiteSpace(author) && !string.IsNullOrWhiteSpace(content))
+                        {
+
+                            NoteClass nyAnteckning = new NoteClass
+                            {
+
+                                Author = author,
+                                Content = content
+
+                            };
+
+                            dagbok.addNote(nyAnteckning);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Author or content can not be empty!");
+                        }
+
+                        break;
+
+
+                    //Rader en note
+                    case 3:
+                        Console.WriteLine("You entered 3. Which note number do you want to remove?");
+                        string noteIndexToBeRemoved = Console.ReadLine();
+
+                        if (int.TryParse(noteIndexToBeRemoved, out int inputvalue))
+                        {
+                            dagbok.deleteNote(inputvalue);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Input has to be a number");
+                        }
+
+
+                        break;
+
+
+                    //Existing program
+                    case 4:
+                        Console.WriteLine("Exiting program. Goodbye!");
+                        return;
+
+
+                    //Default value
+
+                    default:
+                        Console.WriteLine("Invalid input. Please enter a number between 1 and 4.");
+                        break;
+                }
+
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Input has to be a number");
+            }
+             Console.WriteLine("\n");
+            Console.WriteLine("What do want to do now? ");
+
+            showOptions();
+        }
+
+
+    }
+}
---
+++ dagbok.cs
@@ -0,0 +1,97 @@
+using System.Text.Json;
+
+
+namespace Dagbok;
+
+
+public class NoteClass
+{
+    public string Author { get; set; }
+    public string Content { get; set; }
+}
+
+
+class DagBokClass
+{
+
+// Skapa en lista med getter och setters. Listan är av typen objekt
+    public List<NoteClass> Notes { get; set; }
+
+    string fileName = "notedblist.json";
+    string jsonContent;
+
+
+    public DagBokClass()
+    {
+        converJsonFileToList();
+    }
+
+
+    public void converJsonFileToList()
+    {
+
+        if (!File.Exists(fileName))
+        {
+            // If the file doesn't exist, create a new empty JSON file
+            File.WriteAllText(fileName, "[]");
+        }
+        //Read all content of json file. put into string.
+        jsonContent = File.ReadAllText(fileName);
+
+        // Deserialize the JSON content into a list of NoteClass objects
+        Notes = System.Text.Json.JsonSerializer.Deserialize<List<NoteClass>>(jsonContent);
+
+        // If Notes is still null, initialize a new list
+        Notes ??= new List<NoteClass>();
+
+    }
+
+
+    public void addNote(NoteClass anteckning)
+    {
+
+        Notes.Add(anteckning);
+
+        string jsonString = JsonSerializer.Serialize(Notes);
+
+        File.WriteAllText(fileName, jsonString);
+
+        Console.WriteLine("new note has been added!");
+
+    }
+
+
+
+    public void deleteNote(int noteIndex)
+    {
+        if (noteIndex > Notes.Count - 1)
+        {
+            Console.WriteLine($"Erorr! value must be a a number between 0 and {Notes.Count - 1}");
+        }
+
+        else
+        {
+            Notes.RemoveAt(noteIndex);
+            string jsonString = JsonSerializer.Serialize(Notes);
+
+            File.WriteAllText(fileName, jsonString);
+            Console.WriteLine($"note with index {noteIndex} succesfully removed!");
+        }
+
+
+    }
+
+    public void showAllNotes()
+    {
+
+        int indexOfNote = 0;
+
+        foreach (var n in Notes)
+        {
+            Console.WriteLine($"[{indexOfNote}] - Av: {n.Author} ");
+            Console.WriteLine($"{n.Content} \n");
+            indexOfNote++;
+        }
+    }
+
+}
